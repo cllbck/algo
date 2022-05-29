76106f70fdfc62b5f6a644060a024b76cb7d1517
Evgeniy <noname4ik@gmail.com>
2022-05-29 15:57:26 +0300

    add selection sort and heap sort

--- sort/main.go
+++ sort/main.go
@@ -18,6 +18,8 @@ var cases = []caseFunc{
 	{"bubbleSort", bubbleSort},
 	{"insertionSort", insertionSort},
 	{"shellSort", shellSort},
+	{"selectionSort", selectionSort},
+	{"heapSort", heapSort},
 }
 
 func bubbleSort(arr []int) {
@@ -54,6 +56,50 @@ func shellSort(arr []int) {
 	}
 }
 
+func selectionSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		min := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[min] {
+				min = j
+			}
+		}
+		if min != i {
+			arr[i], arr[min] = arr[min], arr[i]
+		}
+	}
+}
+
+func heapSort(arr []int) {
+	for i := len(arr)/2 - 1; i >= 0; i-- {
+		heapify(arr, i, len(arr))
+	}
+	for i := len(arr) - 1; i > 0; i-- {
+		arr[0], arr[i] = arr[i], arr[0]
+		heapify(arr, 0, i)
+	}
+}
+
+func heapify(arr []int, i, n int) {
+	for i >= 0 {
+		largest := i
+		l := 2*i + 1
+		r := 2*i + 2
+		if l < n && arr[l] > arr[largest] {
+			largest = l
+		}
+		if r < n && arr[r] > arr[largest] {
+			largest = r
+		}
+		if largest != i {
+			arr[i], arr[largest] = arr[largest], arr[i]
+			i = largest
+		} else {
+			break
+		}
+	}
+}
+
 func generateRandomArray(maxSize, maxValue int) []int {
 	arr := make([]int, maxSize)
 	for i := 0; i < maxSize; i++ {
@@ -66,15 +112,36 @@ func generateRandomArrays(maxSize, maxValue int) [][]int {
 	arr1 := generateRandomArray(maxSize, maxValue)
 	arr2 := make([]int, maxSize)
 	arr3 := make([]int, maxSize)
+	arr4 := make([]int, maxSize)
+	arr5 := make([]int, maxSize)
 	copy(arr2, arr1)
 	copy(arr3, arr1)
-	return [][]int{arr1, arr2, arr3}
+	copy(arr4, arr1)
+	copy(arr5, arr1)
+	return [][]int{arr1, arr2, arr3, arr4, arr5}
 }
 
 func timeTrackWithResp(f func(number []int), n []int) string {
 	start := time.Now()
 	f(n)
 	return fmt.Sprintf("%s", time.Since(start))
+}
+
+func deadLineWrapper(f func(number []int), n []int) string {
+	ch := make(chan string)
+	ticker := time.NewTicker(2 * time.Minute)
+
+	go func() {
+		ch <- timeTrackWithResp(f, n)
+	}()
+	for {
+		select {
+		case ret := <-ch:
+			return ret
+		case <-ticker.C:
+			return "timeout"
+		}
+	}
 }
 
 func main() {
@@ -85,12 +152,12 @@ func main() {
 		header = append(header, c.title)
 	}
 	rows := []table.Row{}
-	for _, i := range []int{100, 1000, 10000} {
+	for _, i := range []int{100, 1000, 10000, 100000, 1000000} {
 		row := table.Row{i}
 		arrays := generateRandomArrays(i, 1000000)
 		useArray := 0
 		for _, c := range cases {
-			row = append(row, fmt.Sprintf("%s", timeTrackWithResp(c.f, arrays[useArray])))
+			row = append(row, fmt.Sprintf("%s", deadLineWrapper(c.f, arrays[useArray])))
 			useArray++
 		}
 		rows = append(rows, row)
